3e54ae3e02c5b9c05db63cdffded34a0c56893dc
illia <shabelnyk.ilya@gmail.com>
2020-04-22 16:09:00 +0300

    feat(navbar): add navbar for registered users with links (create, list-links and logout). When user is logout from app, him redirect on main page with form login (and register).

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import 'materialize-css'
 import { useRoutes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContext } from './context/auth.context'
+import { useAuth } from './hooks/auth.hook'
+import { Navbar } from './components/navbar'
+
+import 'materialize-css'
 
 function App() {
-  const routes = useRoutes(false)
-  return (
-      <div className="container">
-        {routes}
-      </div>
-  )
+    const { token, login, logout, userId } = useAuth()
+    const isAuthenticated = !!token
+    const routes = useRoutes(isAuthenticated)
+    return (
+        <AuthContext.Provider value={{ token, login, logout, userId, isAuthenticated }}>
+            <BrowserRouter>
+                {isAuthenticated && <Navbar />}
+                <div className="container">
+                    {routes}
+                </div>
+            </BrowserRouter>
+        </AuthContext.Provider>
+
+    )
 }
 
 export default App
