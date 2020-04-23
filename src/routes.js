e7efa9f8e51df94f7d33e26e7a60470d516c2f1c
illia <shabelnyk.ilya@gmail.com>
2020-04-23 13:37:09 +0300

    feat(detail page): add to detail-page.js - logic for get link and get all information about it by id. Add loader when the request is executed.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { LinksPage } from './pages/links-page'
 import { CreatePage } from './pages/create-page'
-import { DetailsPage } from './pages/details-page'
+import { DetailPage } from './pages/detail-page'
 import { AuthPage } from './pages/auth-page'
 
 export const useRoutes = isAuthenticated => {
@@ -11,7 +11,7 @@ export const useRoutes = isAuthenticated => {
             <Switch>
                 <Route path='/links' exact component={LinksPage} />
                 <Route path='/create' exact component={CreatePage} />
-                <Route path='/detail/:id' exact component={DetailsPage} />
+                <Route path='/detail/:id' exact component={DetailPage} />
                 <Redirect to='/create' />
             </Switch>
         )
